0be477d3f388f60efad16a8dea0423440e9867bc
Jahidul Islam Jihad <islamjahiduljihad@gmail.com>
2025-06-24 00:28:48 +0600

    Navbar updated.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
+    const list = (
+        <>
+            <li>
+                <Link to={"/"}>Home</Link>
+            </li>
+            <li>
+                <Link to={"/about"}>About</Link>
+            </li>
+            <li>
+                <Link to={"https://jahiduljihad.netlify.app/"}>Developer</Link>
+            </li>
+        </>
+    );
     return (
         <div className="navbar bg-[#00b795] shadow-sm md:px-10 lg:px-20">
             <div className="flex-1">
@@ -7,11 +22,7 @@ const Navbar = () => {
                 </a>
             </div>
             <div className="flex-none">
-                <ul className="menu menu-horizontal px-1">
-                    <li>
-                        <a>Home</a>
-                    </li>
-                </ul>
+                <ul className="menu menu-horizontal px-1">{list}</ul>
             </div>
         </div>
     );
